e227122f29f41a874769b292f3f9a9e987eeaf9f
Ali Sinan Coban <alisinancoban@gmail.com>
2019-03-21 19:42:50 +0300

    arrow function basics

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,35 @@
-let firstName = "sinan";
-let lastname = "coban";
+const numbers = [1,2,3,4,5];
 
-function calculateAge(year){
-    return 2019 - year;
-}
-
-//ES5 syntax 
-console.log("I am " + firstName + " " +  lastname);
+//ES5 syntax
+var es5 = numbers.map(function(current){
+   return current; 
+});
 
 //ES6 syntax
-console.log(`I am ${firstName} ${lastname} ${calculateAge(1995)}`);
+var es6 = numbers.map(current => current);
+var es6 = numbers.map((current, index) => {
+    return `${current} index is ${index}`;
+});
+
+// ------------------- Arrow function 2 this -----------------
+var es5 = {
+    color: "green",
+    position: 1,
+    click: function(){
+        var self = this;//if you can`t do that this refers to the global object
+        document.querySelector(".green").addEventListener("click", function(){
+            alert(self.color + self.position);
+        });
+    }
+};
+
+var es6 = {
+    color: "green",
+    position: 1,
+    click: function(){
+        document.querySelector(".green").addEventListener("click", () => {
+            alert(this.color + this.position);
+        });
+    }
+};
+
