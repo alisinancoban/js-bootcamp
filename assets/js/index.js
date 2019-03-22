cddfbd6ecd36bbe30e0818c88667088605d4302d
Ali Sinan Coban <alisinancoban@gmail.com>
2019-03-22 21:41:35 +0300

    Inheritance

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,22 @@ function Person5(name, lastname, birthYear){
 }
 
 Person5.prototype.calculateAge = function(){
-    return new Date().getFullYear - this.birthYear;
+    return new Date().getFullYear() - this.birthYear;
 };
 
 var person5 = new Person5("name", "lastname", 22);
+
+var Engineer5 = function(name, lastname, birthYear, job){
+    Person5.call(this, name, lastname, birthYear);
+    this.job = job;
+};
+//Inheritance is here
+Engineer5.prototype = Object.create(Person5.prototype);
+//Person can not access
+Engineer5.prototype.solveProblem = function(){console.log("Problem solved");}
+var engineer5 = new Engineer5("name", "lastname", 1995, "software eng.");
+engineer5.calculateAge();
+
 //ES6 syntax
 class Person6{
     constructor(name, lastname, birthYear){
@@ -19,7 +31,7 @@ class Person6{
     }
 
     calculateAge(){
-        return new Date().getFullYear - this.birthYear;
+        return new Date().getFullYear() - this.birthYear;
     }
 
     static greeting(){
@@ -29,4 +41,18 @@ class Person6{
 
 var person6 = new Person6("name", "lastname", 22);
 Person6.greeting();
+
+class Engineer6 extends Person6{
+    constructor(name, lastname, birthYear, job){
+        super(name, lastname, birthYear);
+        this.job = job;
+    }
+
+    solveProblem(){
+        console.log("Problem solved");
+    }
+}
+
+const engineer6 = new Engineer6("name", "lastname", 1995, "software eng.");
+engineer6.calculateAge();
 
