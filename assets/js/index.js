8e48dc2c657b6d92843995277619ee0243e2b3ed
Ali Sinan Coban <alisinancoban@gmail.com>
2019-03-24 20:56:22 +0300

    closure and moduile pattern

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,58 +1,40 @@
-//ES5 syntax
-function Person5(name, lastname, birthYear){
-    this.name = name;
-    this.lastname = lastname;
-    this.birthYear = birthYear;
+//Closure
+function makeAdder(value){
+    //inner function add() uses `value` so it has a closure over it
+    function add(number){
+        return number + value;
+    };
+    return add;
 }
 
-Person5.prototype.calculateAge = function(){
-    return new Date().getFullYear() - this.birthYear;
-};
+var plusOne = makeAdder(1);
+var plusTen = makeAdder(10);
 
-var person5 = new Person5("name", "lastname", 22);
+console.log(plusOne(3)); // 4
+console.log(plusTen(3)); // 13
 
-var Engineer5 = function(name, lastname, birthYear, job){
-    Person5.call(this, name, lastname, birthYear);
-    this.job = job;
-};
-//Inheritance is here
-Engineer5.prototype = Object.create(Person5.prototype);
-//Person can not access
-Engineer5.prototype.solveProblem = function(){console.log("Problem solved");}
-var engineer5 = new Engineer5("name", "lastname", 1995, "software eng.");
-engineer5.calculateAge();
 
-//ES6 syntax
-class Person6{
-    constructor(name, lastname, birthYear){
-        this.name = name;
-        this.lastname = lastname;
-        this.birthYear = birthYear;
+function User(){
+    var username, password;
+
+    function doLogin(user, pw){
+        username = user;
+        password = pw;
+
+        if(user === "sinan" && pw === "123"){
+            console.log("corrent")
+        }
+        else{
+            console.log("false");
+        }
     }
+    
+    var publicAPI = {
+        login: doLogin
+    };
 
-    calculateAge(){
-        return new Date().getFullYear() - this.birthYear;
-    }
-
-    static greeting(){
-        console.log("Hello");
-    }
+    return publicAPI;
 }
 
-var person6 = new Person6("name", "lastname", 22);
-Person6.greeting();
-
-class Engineer6 extends Person6{
-    constructor(name, lastname, birthYear, job){
-        super(name, lastname, birthYear);
-        this.job = job;
-    }
-
-    solveProblem(){
-        console.log("Problem solved");
-    }
-}
-
-const engineer6 = new Engineer6("name", "lastname", 1995, "software eng.");
-engineer6.calculateAge();
-
+var fred = User();
+fred.login("sinan", "1233");
