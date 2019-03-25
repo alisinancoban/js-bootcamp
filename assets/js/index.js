1ba1d3bb3f7739a9052adf5eeab45c05cfada756
Ali Sinan Coban <alisinancoban@gmail.com>
2019-03-25 19:57:37 +0300

    prototypes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,9 @@
-//Closure
-function makeAdder(value){
-    //inner function add() uses `value` so it has a closure over it
-    function add(number){
-        return number + value;
-    };
-    return add;
-}
-
-var plusOne = makeAdder(1);
-var plusTen = makeAdder(10);
-
-console.log(plusOne(3)); // 4
-console.log(plusTen(3)); // 13
-
-
-function User(){
-    var username, password;
-
-    function doLogin(user, pw){
-        username = user;
-        password = pw;
-
-        if(user === "sinan" && pw === "123"){
-            console.log("corrent")
-        }
-        else{
-            console.log("false");
-        }
-    }
-    
-    var publicAPI = {
-        login: doLogin
-    };
-
-    return publicAPI;
-}
-
-var fred = User();
-fred.login("sinan", "1233");
+var foo = {
+    a: 42
+   };
+   
+   // create `bar` and link it to `foo`
+   var bar = Object.create( foo );
+   bar.b = "hello world";
+   bar.b; // "hello world"
+   bar.a; // 42 <-- delegated to `foo`
