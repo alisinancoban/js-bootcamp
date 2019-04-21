7b2e30555c5ba8ecc7409a3e4e0edf443c69cddb
Ali Sinan Coban <alisinancoban@gmail.com>
2019-04-21 16:22:39 +0300

    loops and closure(block scope)

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,4 +26,17 @@ It needs its own variable, with a copy of the i value at each iteration. */
     }, j*1000 );
     })( i );
    }
+
+/*Look carefully at our analysis of the previous solution. We used an
+IIFE to create new scope per-iteration. In other words, we actually
+needed a per-iteration block scope let declaration hijacks a 
+block and declares a variable right there in the block.
+It essentially turns a block into a scope that we can close over. So, the
+following awesome code just works: */
+   for (let i=1; i<=5; i++) {
+    setTimeout( function timer(){
+    console.log( i );
+    }, i*1000 );
+   }
+
    
